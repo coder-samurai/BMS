d9c77ae24eda7f1207b577703c8e2b8f0c7985b2
anas <88755734+coder-samurai@users.noreply.github.com>
2023-02-10 12:52:18 +0100

    add class BMS

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# modules
+import os
+import random
+import string
+
+
+# bank managment system | bms
+class BMS:
+
+    def __init__(self):
+        pass
+    
+    def HomePage(self):
+        pass
+    
+    def PasswordGenerator(self, Number=8):
+        pass
+
+    def Login(self):
+        pass
+    
+    def SignUp(self):
+        pass
+
+
+# run the system
+if __name__ == "__main__":
+    BMS()
